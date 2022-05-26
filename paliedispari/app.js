a98d267918d927764efed7285162903c6d9c69db
EnricoPiombini <piombini.enrico@gmail.com>
2022-05-26 16:29:13 +0200

    random number function

--- paliedispari/app.js
+++ paliedispari/app.js
@@ -4,3 +4,20 @@
 // Sommiamo i due numeri (quello inserito dall’utente e quello random del computer)
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto. Se l’utente aveva scelto pari e la somma è pari, ha vinto l’utente, altrimenti il computer
+
+const sceltaPariDispari = prompt('Inserisci pari o dispari');
+console.log(sceltaPariDispari);
+
+
+// Generiamo un numero random per il pc da 1 a 5
+function numberCpu(min, max) {
+    const randomCpu = Math.floor(Math.random()*(max- min + 1)) + min;
+   return randomCpu;
+
+};
+console.log(numberCpu(1,5))
+
+
+
+
+
